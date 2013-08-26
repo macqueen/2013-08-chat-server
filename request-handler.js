45a16c8b26d33be352030d53ac2da6391ca09a5c
Hack Reactor Students <github@hackreactor.com>
2013-08-26 15:50:55 -0700

    write our own request handler, export frpm request handler and require to basic-server. began implementing chat urls

--- request-handler.js
+++ request-handler.js
@@ -4,6 +4,34 @@
  * from this file and include it in basic-server.js. Check out the
  * node module documentation at http://nodejs.org/api/modules.html. */
 
+/* This is the callback function that will be called each time a
+ * client (i.e.. a web browser) makes a request to our server. */
+
 var handleRequest = function(request, response) {
+  console.log("Serving request type " + request.method + " for url " + request.url);
+  var statusCode = 200;
+  var headers = defaultCorsHeaders;
+  headers['Content-Type'] = "text/plain";
+  response.writeHead(statusCode, headers);
+  if(request.url === "/1/classes/messages") {
+    response.end("great job");
+  }else{
+    response.end('your request makes no sense to me');
+  }
 
 };
+
+/* These headers will allow Cross-Origin Resource Sharing.
+ * This CRUCIAL code allows this server to talk to websites that
+ * are on different domains. (Your chat client is running from a url
+ * like file://your/chat/client/index.html, which is considered a
+ * different domain.) */
+
+var defaultCorsHeaders = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
+};
+
+module.exports = handleRequest;
